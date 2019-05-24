6af208d309fee5b1ca5c2bb7c61c4d8c32750cb6
Ming Liu <mliu@macpro.home>
2019-05-24 12:46:28 -0400

    update links for amazon and b&n for purchasing modal

--- bookr/src/components/protected/books/BuyBookModal.js
+++ bookr/src/components/protected/books/BuyBookModal.js
@@ -14,7 +14,20 @@ class BuyBookModal extends React.Component {
         };
     }
 
-  
+    convertISBN = isbn => {
+        let total = 0;
+       
+        let result = isbn.substring(3, 12);
+        for (let x = 0; x < 9; x += 1) {
+          total = total + (result.charAt(x) * (10 - x));
+        }
+       
+        let z = (11 - (total % 11)) % 11;
+        if (z === 10) { z = "X"; }
+       
+        return result + z;
+       }
+       
 
   handleClickOpen = () => {
     this.setState({ open: true });
@@ -40,11 +53,20 @@ class BuyBookModal extends React.Component {
           <DialogContent>
             <DialogContentText id="alert-dialog-description">
                 <div className="bookstore_logo">
-                    <a href={`https://www.amazon.com/s?k=${this.props.isbn}`} target="_blank">
+                    <a href={`https://www.amazon.com/dp/product/${this.convertISBN(this.props.isbn.toString())}`} target="_blank">
                         <img src="https://pmcvariety.files.wordpress.com/2018/01/amazon-logo.jpg?w=700&h=393&crop=1"></img>
                     </a>
+                    
                 </div>
+                <div className="bookstore_logo">
+                    <a href={`https://www.barnesandnoble.com/w?ean=${this.props.isbn}#/`} target="_blank">
+                        <img src="https://radioimg.s3.amazonaws.com/wdokfm/styles/delta__775x515/s3/barnes-and-noble-logo.png?YSgehDKHmKRdwoT4XywcYc0VWAPvesuY&itok=mJDcjSSM"></img>
+                    </a>
+                    
+                </div>
+                <p>All designated trademarks and brands are the property of their respective owners.</p>
             </DialogContentText>
+            
           </DialogContent>
           <DialogActions>
             <Button onClick={this.handleClose} color="primary">
